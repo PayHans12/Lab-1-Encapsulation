dddc69e3d7e5cc1227d9af4f85544cc538312718
Payton Hansen1 <payton.hansen1@students.snow.edu>
2025-01-14 14:55:54 -0700

    Mostly finished

---
+++ Bank.cs
@@ -0,0 +1,55 @@
+namespace Bank;
+
+public class BankAccount
+{
+    private string accountNumber;
+    private decimal balance;
+    public string AccountNumber { get; }
+    public decimal Balance { get; }
+
+    public BankAccount(string accountNumber, decimal initialbalance)
+    {
+        this.accountNumber = AccountNumber = accountNumber;
+        if (initialbalance < 0)
+        {
+            initialbalance = 0;
+        }
+        balance = Balance = initialbalance;
+    }
+
+    public void Deposit(decimal amount)
+    {
+        if (amount > 0)
+        {
+            balance += amount;
+            Console.WriteLine($"Depositing: ${amount}");
+            Console.WriteLine($"New Balance: ${balance}\n");
+        }
+        else
+        {
+            Console.WriteLine("Error: that is an invalid amount.");
+        }
+    }
+
+    public void Withdraw(decimal amount)
+    {
+        if (amount > 0 && amount <= balance)
+        {
+            balance -= amount;
+            Console.WriteLine($"Withdrawing: ${amount}");
+            Console.WriteLine($"New Balance: ${balance}\n");
+        }
+        else
+        {
+            if (amount > balance)
+            {
+                Console.WriteLine("Error: Insuficient Funds");
+            }
+            else
+            {
+                Console.WriteLine("Invalid amount");
+            }
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using Bank;
+Console.Clear();
+
+BankAccount phil = new BankAccount("123456", (decimal)500.00);
+Console.WriteLine($"Account Number: {phil.AccountNumber}");
+Console.WriteLine($"Current Balance: {phil.Balance}\n");
+
+phil.Deposit((decimal)100.00);
+
+phil.Withdraw((decimal)200.00);
+
+phil.Withdraw((decimal)1000.00);
+
